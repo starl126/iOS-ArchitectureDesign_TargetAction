f91d5ae8e8a705f4909510b5f94d3c8e60f718a0
刘欣 <starxing2005@126.com>
2019-12-03 11:51:27 +0800

    修改文件头

--- TargetActionDemo/TargetActionDemo/Configuration/LXAppearance.m
+++ TargetActionDemo/TargetActionDemo/Configuration/LXAppearance.m
@@ -66,7 +66,7 @@
     if (@available(iOS 13.0, *)) {
         
         SEL originalSel = @selector(presentViewController:animated:completion:);
-        SEL swizzledSel = @selector(LQ_presentViewController:animated:completion:);
+        SEL swizzledSel = @selector(LX_presentViewController:animated:completion:);
         
         Method originalMethod = class_getInstanceMethod(self, originalSel);
         Method swizzledMethod = class_getInstanceMethod(self, swizzledSel);
@@ -79,12 +79,12 @@
         }
     }
 }
-- (void)LQ_presentViewController:(UIViewController *)viewController animated:(BOOL)animated completion:(void (^)(void))completion API_AVAILABLE(ios(13.0)) {
+- (void)LX_presentViewController:(UIViewController *)viewController animated:(BOOL)animated completion:(void (^)(void))completion API_AVAILABLE(ios(13.0)) {
     if (viewController.modalPresentationStyle == UIModalPresentationPageSheet || viewController.modalPresentationStyle == UIModalPresentationAutomatic) {
         viewController.modalPresentationStyle = UIModalPresentationFullScreen;
     }
     viewController.modalPresentationCapturesStatusBarAppearance = YES;
-    [self LQ_presentViewController:viewController animated:animated completion:completion];
+    [self LX_presentViewController:viewController animated:animated completion:completion];
 }
 
 @end
